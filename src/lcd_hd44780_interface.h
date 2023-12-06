bf416c731d0e90c5dd3b62ac2f80f7a535dace3b
niwciu <niwciu@gmail.com>
2023-12-06 22:31:07 +0100

    LCD: define library interface - this comes out form the assumptions after studying the datasheet of the LCD driver

--- src/lcd_hd44780_interface.h
+++ src/lcd_hd44780_interface.h
@@ -1,8 +1,8 @@
 /*
- * @Author: lukasz.niewelt 
- * @Date: 2023-12-06 21:38:59 
- * @Last Modified by:   lukasz.niewelt 
- * @Last Modified time: 2023-12-06 21:38:59 
+ * @Author: lukasz.niewelt
+ * @Date: 2023-12-06 21:38:59
+ * @Last Modified by: lukasz.niewelt
+ * @Last Modified time: 2023-12-06 22:31:49
  */
 
 #ifndef _LCD_HD44780_INTERFACE_H_
@@ -12,10 +12,39 @@
 extern "C"
 {
 #endif /* __cplusplus */
+#include <stdint.h>
+    enum lcd_sig
+    {
+        LCD_RS,
+        LCD_RW,
+        LCD_E,
+    };
 
+    typedef void (*init_LCD_data_and_SIG_pins_func_p)(void);
+    typedef void (*set_LCD_data_pins_as_outputs_func_p)(void);
+    typedef void (*set_LCD_data_pins_as_inputs_func_p)(void);
+    typedef void (*set_LCD_data_port_func_p)(uint8_t data);
+    typedef uint8_t (*get_LCD_data_port_func_p)(void);
+    typedef void (*init_LCD_SIG_func_p)(void);
+    typedef void (*set_LCD_SIG_func_p)(enum lcd_sig LCD_SIG);
+    typedef void (*reset_LCD_SIG_func_p)(enum lcd_sig LCD_SIG);
+    typedef void (*delay_us_func_p)(uint32_t delay_us);
 
-
-
+    struct LCD_IO_driver_interface_struct
+    {
+        init_LCD_data_and_SIG_pins_func_p init_LCD_pins;
+        set_LCD_data_pins_as_outputs_func_p set_data_pins_as_outputs;
+        set_LCD_data_pins_as_inputs_func_p init_data_pins_as_inputs;
+        set_LCD_data_port_func_p write_data;
+        get_LCD_data_port_func_p read_data;
+        init_LCD_SIG_func_p init_SIG;
+        set_LCD_SIG_func_p set_SIG;
+        reset_LCD_SIG_func_p reset_SIG;
+        delay_us_func_p delay_us;
+    };
+    // clang-format off
+    const struct LCD_IO_driver_interface_struct* LCD_IO_driver_interface_get(void);
+    // clang-format on
 
 #ifdef __cplusplus
 }
