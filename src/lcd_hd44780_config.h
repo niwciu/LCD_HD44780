51632462f3219a55941be2db0d433ca851fd528e
niwciu <niwciu@gmail.com>
2023-12-06 22:09:48 +0100

    LCD: implement configuration infrastructure

--- src/lcd_hd44780_config.h
+++ src/lcd_hd44780_config.h
@@ -1,8 +1,8 @@
 /*
  * @Author: lukasz.niewelt 
  * @Date: 2023-12-06 21:38:27 
- * @Last Modified by:   lukasz.niewelt 
- * @Last Modified time: 2023-12-06 21:38:27 
+ * @Last Modified by: lukasz.niewelt
+ * @Last Modified time: 2023-12-06 22:08:56
  */
 
 #ifndef _LCD_HD44780_CONFIG_H_
@@ -13,7 +13,10 @@ extern "C"
 {
 #endif /* __cplusplus */
 
+#define ON 1
+#define OFF 0
 
+#define USE_RW_PIN ON
 
 
 
