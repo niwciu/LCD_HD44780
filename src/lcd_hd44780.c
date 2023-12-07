8199d20df4c20b871078be5e6da1b7d4ab00c586
niwciu <niwciu@gmail.com>
2023-12-07 14:07:15 +0100

    LCD: REFACTOR add lcd cmd defines

--- src/lcd_hd44780.c
+++ src/lcd_hd44780.c
@@ -2,10 +2,45 @@
  * @Author: lukasz.niewelt
  * @Date: 2023-12-06 21:39:30
  * @Last Modified by: lukasz.niewelt
- * @Last Modified time: 2023-12-07 13:48:35
+ * @Last Modified time: 2023-12-07 14:09:56
  */
 
 #include "lcd_hd44780.h"
+
+#define BUSY_FLAG 1 << 7
+
+// LCD driver commands
+// clang-format off
+#define LCDC_CLS            0x01
+#define LCDC_HOME           0x02
+/********************************/
+#define LCDC_ENTRY_MODE     0x04
+    #define LCDC_ENTRYR     0x02
+    #define LCDC_ENTRYL     0x00
+    #define LCDC_MOVE       0x01
+/********************************/
+#define LCDC_ONOFF          0x08
+    #define LCDC_DISPLAYON  0x04
+    #define LCDC_CURSORON   0x02
+    #define LCDC_CURSOROFF  0x00
+    #define LCDC_BLINKON    0x01
+/********************************/
+#define LCDC_SHIFT          0x10
+    #define LCDC_SHIFTDISP  0x08
+    #define LCDC_SHIFTR     0x04
+    #define LCDC_SHIFTL     0x00
+/********************************/
+#define LCDC_FUNC           0x20
+    #define LCDC_FUNC8B     0x10
+    #define LCDC_FUNC4B     0x00
+    #define LCDC_FUNC2L     0x08
+    #define LCDC_FUNC1L     0x00
+    #define LCDC_FUNC5x10   0x04
+    #define LCDC_FUNC5x7    0x00
+/********************************/
+#define LCDC_SET_CGRAM      0x40
+#define LCDC_SET_DDRAM      0x80
+// clang-format on
 
 static const struct LCD_IO_driver_interface_struct *LCD = NULL;
 
--- test/lcd_hd44780/mock_LCD_IO_driver.h
+++ test/lcd_hd44780/mock_LCD_IO_driver.h
@@ -2,7 +2,7 @@
  * @Author: lukasz.niewelt 
  * @Date: 2023-12-06 22:11:42 
  * @Last Modified by: lukasz.niewelt
- * @Last Modified time: 2023-12-07 00:25:21
+ * @Last Modified time: 2023-12-07 14:10:39
  */
 #include <stdint.h>
 #define BUF_SIZE 1000
@@ -15,6 +15,38 @@
 #define mock_LCD_E 1 << 2
 #define mock_LCD_RS 1 << 0
 #define mock_LCD_RW 1 << 1
+
+// clang-format off
+#define LCDC_CLS            0x01
+#define LCDC_HOME           0x02
+/********************************/
+#define LCDC_ENTRY_MODE     0x04
+    #define LCDC_ENTRYR     0x02
+    #define LCDC_ENTRYL     0x00
+    #define LCDC_MOVE       0x01
+/********************************/
+#define LCDC_ONOFF          0x08
+    #define LCDC_DISPLAYON  0x04
+    #define LCDC_CURSORON   0x02
+    #define LCDC_CURSOROFF  0x00
+    #define LCDC_BLINKON    0x01
+/********************************/
+#define LCDC_SHIFT          0x10
+    #define LCDC_SHIFTDISP  0x08
+    #define LCDC_SHIFTR     0x04
+    #define LCDC_SHIFTL     0x00
+/********************************/
+#define LCDC_FUNC           0x20
+    #define LCDC_FUNC8B     0x10
+    #define LCDC_FUNC4B     0x00
+    #define LCDC_FUNC2L     0x08
+    #define LCDC_FUNC1L     0x00
+    #define LCDC_FUNC5x10   0x04
+    #define LCDC_FUNC5x7    0x00
+/********************************/
+#define LCDC_SET_CGRAM      0x40
+#define LCDC_SET_DDRAM      0x80
+// clang-format on
 
 extern uint8_t mock_LCD_DATA_PORT;
 extern uint8_t mock_LCD_SIG_PORT;
