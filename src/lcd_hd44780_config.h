d3f553c9411252ebfb0b37dfa3cb14616f41e647
niwciu <niwciu@gmail.com>
2023-12-08 10:45:12 +0100

    Project: REFACTOR

--- src/lcd_hd44780_config.h
+++ src/lcd_hd44780_config.h
@@ -2,7 +2,7 @@
  * @Author: lukasz.niewelt 
  * @Date: 2023-12-06 21:38:27 
  * @Last Modified by: lukasz.niewelt
- * @Last Modified time: 2023-12-08 10:36:36
+ * @Last Modified time: 2023-12-08 10:51:20
  */
 
 #ifndef _LCD_HD44780_CONFIG_H_
@@ -16,7 +16,19 @@ extern "C"
 #define ON 1
 #define OFF 0
 
-#define USE_RW_PIN On
+#define USE_RW_PIN                      OFF // ToDo Enable/Disable RW Pin usage
+
+/********************************  LCD LIBRARY COMPILATION SETTINGS ************************
+ *      Setting USE_(procedure name) to:
+ *          ON  - add specific procedure to complilation
+ *          OFF - exclude specific procedure from complitaion
+********************************************************************************************/
+#define USE_DEF_CHAR_FUNCTION           ON
+
+#define USE_LCD_CURSOR_HOME             ON
+#define USE_LCD_CURSOR_ON               ON
+#define USE_LCD_CURSOR_OFF              ON
+#define USE_LCD_BLINKING_CURSOR_ON      ON
 
 
 
