5d6f1c74fc3ae10399bd3bddc1138ef4b83af9b7
niwciu <niwciu@gmail.com>
2023-12-08 10:35:50 +0100

    Test: RED -> add test logic GivenLcdInitWhenUseLcdBlinkingCursorOnThenSignalSequenceIsCorrect

--- test/lcd_hd44780/lcd_hd44780_test_basic_func.c
+++ test/lcd_hd44780/lcd_hd44780_test_basic_func.c
@@ -95,7 +95,10 @@ TEST(lcd_hd44780_basic_functions, GivenLcdInitWhenUseLcdCursorOffThenSignalSeque
 
 TEST(lcd_hd44780_basic_functions, GivenLcdInitWhenUseLcdBlinkingCursorOnThenSignalSequenceIsCorrect)
 {
-    TEST_FAIL_MESSAGE("Implement your test!");
+    next_log_no = define_expected_sequence_for_send_cmd_to_LCD(0, (LCDC_ONOFF | LCDC_DISPLAYON | LCDC_CURSORON | LCDC_BLINKON), 0x00,0);
+    expected_buf_lenght = (next_log_no) * (LOG_DATA_AMOUNT);
+    lcd_blinking_cursor_on();
+    TEST_ASSERT_EQUAL_UINT16_ARRAY(expected_LCD_Port_delay_dump_data, mock_LCD_Port_delay_dump_data, expected_buf_lenght);
 }
 
 // TEST(lcd_hd44780_basic_functions, )
