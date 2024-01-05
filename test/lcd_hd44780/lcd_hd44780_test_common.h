c2b8aecccc0319deec66fd3d87948fa67785c71f
niwciu <57457942+niwciu@users.noreply.github.com>
2024-01-06 00:26:30 +0100

    Feature/cppcheck fixes (#11)
    * ReadMe: change title, add features description
    
    * Workflows: change to latest checkout version and update trigers
    
    * Test: REFACTOR
    
    const correctnes
    
    * Test: REFACTOR
    
    cppcheck ->shadow variable
    
    * Test: REFACTOR
    
    cppcheck -> tab index out of range
    
    * Test: REFACTOR
    
    cppcheck -> unused parameter in function
    
    * Test: REFACTOR clang-format check
    
    * Workflow: switch off unused workflows
    
    * Workflows: improce cppcheck workflow

--- test/lcd_hd44780/lcd_hd44780_test_common.h
+++ test/lcd_hd44780/lcd_hd44780_test_common.h
@@ -17,10 +17,17 @@ extern uint16_t expected_LCD_Port_delay_dump_data[BUF_SIZE][LOG_DATA_AMOUNT];
 extern uint16_t expected_buf_lenght;
 
 void clear_expected_LCD_Port_delay_dump_data(void);
-uint8_t read_prev_LCD_PORT_state(void);
+uint8_t read_prev_LCD_DATA_PORT_state(void);
+uint8_t read_prev_LCD_SIG_PORT_state(void);
 uint16_t define_expected_sequence_for_first_15_ms_delay(void);
 uint16_t define_expected_sequence_for_read_write_4_bit_data(uint16_t log_no, uint8_t R_W_data, uint16_t delay);
-uint16_t define_expected_sequence_for_send_cmd_to_LCD(uint16_t log_no, uint8_t cmd, uint8_t expected_readed_data, uint16_t additional_cmd_delay);
+#if USE_RW_PIN == ON
+uint16_t define_expected_sequence_for_send_cmd_to_LCD(uint16_t log_no, uint8_t cmd, uint8_t expected_readed_data);
 uint16_t define_expected_sequence_for_send_data_to_LCD(uint16_t log_no, uint8_t data, uint8_t expected_readed_data, uint16_t additional_cmd_delay);
+#else
+uint16_t define_expected_sequence_for_send_cmd_to_LCD(uint16_t log_no, uint8_t cmd, uint16_t additional_cmd_delay);
+uint16_t define_expected_sequence_for_send_data_to_LCD(uint16_t log_no, uint8_t data, uint16_t additional_cmd_delay);
+#endif
+
 uint16_t define_expect_sequence_for_lcd_def_char(uint16_t log_no, const uint8_t CGRAM_bank_x_char_index, const uint8_t *def_char);
 uint16_t define_expected_sequence_for_send_string_to_LCD(const char *string);
