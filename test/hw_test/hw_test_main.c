9fa3d879a49205ea211556a04022da1ac6386d93
niwciu <57457942+niwciu@users.noreply.github.com>
2024-01-09 21:10:52 +0100

    HW_Test: GREEN -> 2x16
    tested on arduino lcd 2x16

--- test/hw_test/hw_test_main.c
+++ test/hw_test/hw_test_main.c
@@ -43,6 +43,35 @@ void main_hw_test(void)
     lcd_bin(5, 1);
     lcd_locate(LINE_2, C8);
     lcd_bin(5, 8);
+    _delay_ms(1000);
+    lcd_init();
+    lcd_buf_char('G');
+    lcd_buf_locate(0,2);
+    lcd_buf_str("Test");
+    lcd_buf_print();
+    _delay_ms(1000);
+    lcd_locate(0,0);
+    lcd_buf_char('a');
+    lcd_buf_print();
+    _delay_ms(1000);
+    lcd_buf_cls();
+    #if LCD_TYPE==1602
+    lcd_buf_locate(LINE_1,C16);
+    lcd_buf_str("test1");
+    lcd_buf_locate(LINE_2,C16);
+    lcd_buf_str("test2");
+    #elif LCD_TYPE==2004
+    lcd_buf_locate(LINE_1,C20);
+    lcd_buf_str("test1");
+    lcd_buf_locate(LINE_2,C20);
+    lcd_buf_str("test2");
+    lcd_buf_locate(LINE_3,C20);
+    lcd_buf_str("test3");
+    lcd_buf_locate(LINE_4,C20);
+    lcd_buf_str("test4");
+    #else
+    #endif
+    lcd_buf_print();
     while (1)
         ;
 }
