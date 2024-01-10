353a28d61b4ce0f26e5946f362ebaa344d031f8f
niwciu <57457942+niwciu@users.noreply.github.com>
2024-01-10 14:28:10 +0100

    Test: RED added test infrstructure ->
    GivenLcdBufferingOnAndLcdInitWhenLcdBufCharThenLCD_UPDATE_EVENTflagIsTRUE

--- test/lcd_hd44780/lcd_hd44780_buffering.c
+++ test/lcd_hd44780/lcd_hd44780_buffering.c
@@ -2,7 +2,7 @@
  * @Author: lukasz.niewelt
  * @Date: 2024-01-08 15:45:14
  * @Last Modified by: lukasz.niewelt
- * @Last Modified time: 2024-01-10 14:25:47
+ * @Last Modified time: 2024-01-10 14:27:15
  */
 
 #include "unity/fixture/unity_fixture.h"
@@ -135,7 +135,9 @@ TEST(lcd_hd44780_buffering, GivenLcdBufferingOnAndLcdInitAndLCD_UPDATE_EVENTflag
 
 TEST(lcd_hd44780_buffering, GivenLcdBufferingOnAndLcdInitWhenLcdBufCharThenLCD_UPDATE_EVENTflagIsTRUE)
 {
-   TEST_FAIL_MESSAGE("New Test Added") ;
+    lcd_init();
+    lcd_buf_char('a');
+     TEST_ASSERT_TRUE(LCD_UPDATE_EVENT)
 }
 
 // TEST(lcd_hd44780_buffering, )
