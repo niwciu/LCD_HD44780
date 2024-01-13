3f7e58807b532e3afa1098afcad270d522ab8cc7
niwciu <57457942+niwciu@users.noreply.github.com>
2024-01-13 16:32:25 +0100

    Examples: define _delay_us for stm32G474xx

--- examples/STM32G474RE_NUCLEO_CUBE_IDE/Core/Src/tim_delay.c
+++ examples/STM32G474RE_NUCLEO_CUBE_IDE/Core/Src/tim_delay.c
@@ -5,31 +5,32 @@
  *      Author: niw-lap
  */
 #include "tim_delay.h"
+#include "stm32g474xx.h"
 
 
 
 void _delay_us(uint32_t delay_us)
 {
-	// RCC-> APB1ENR |= RCC_APB1ENR_TIM6EN;
+	RCC-> AHB1ENR |= RCC_APB1ENR1_TIM6EN;
 
-	// TIM6 -> CR1 &=~((uint32_t)(TIM_CR1_CEN));
-	// TIM6 -> PSC = 0;
-	// TIM6 -> ARR =47;
+	TIM6 -> CR1 &=~((uint32_t)(TIM_CR1_CEN));
+	TIM6 -> PSC = 0;
+	TIM6 -> ARR =47;
 
-	// TIM6 -> CR1 |= TIM_CR1_ARPE;
-	// TIM6 -> EGR |= TIM_EGR_UG;
-	// TIM6 -> SR &=~((uint32_t)(TIM_SR_UIF));
+	TIM6 -> CR1 |= TIM_CR1_ARPE;
+	TIM6 -> EGR |= TIM_EGR_UG;
+	TIM6 -> SR &=~((uint32_t)(TIM_SR_UIF));
 
-	// TIM6 -> CR1 |= TIM_CR1_CEN;
-	// while (delay_us!=0)
-	// {
-	// 	if ((TIM6 -> SR & TIM_SR_UIF) == TIM_SR_UIF)
-	// 	{
-	// 		TIM6 -> SR &=~((uint32_t)(TIM_SR_UIF));
-	// 		delay_us--;
-	// 	}
-	// }
-	// TIM6 -> CR1 &=~((uint32_t)(TIM_CR1_CEN));
-	// RCC-> APB1ENR &= ~((uint32_t)RCC_APB1ENR_TIM6EN);  //wy��cz clocka dla timera
+	TIM6 -> CR1 |= TIM_CR1_CEN;
+	while (delay_us!=0)
+	{
+		if ((TIM6 -> SR & TIM_SR_UIF) == TIM_SR_UIF)
+		{
+			TIM6 -> SR &=~((uint32_t)(TIM_SR_UIF));
+			delay_us--;
+		}
+	}
+	TIM6 -> CR1 &=~((uint32_t)(TIM_CR1_CEN));
+	RCC-> AHB1ENR &= ~((uint32_t)RCC_APB1ENR1_TIM6EN);  //wy��cz clocka dla timera
 }
 
