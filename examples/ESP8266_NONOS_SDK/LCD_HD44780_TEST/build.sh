5a1d906b5ee75342c74549f53b55c4eedf5b814a
niwciu <niwciu@gmail.com>
2025-02-26 19:46:37 +0100

    EXAMPLES:  added build ans clean bash script for ESP8266 example

--- examples/ESP8266_NONOS_SDK/LCD_HD44780_TEST/build.sh
+++ examples/ESP8266_NONOS_SDK/LCD_HD44780_TEST/build.sh
@@ -1,7 +1,41 @@
-#!/bin/bash -x
-echo "make_lib.sh version 20160307"
+#!/bin/bash
 
-# $env:PATH = "C:/msys32/xtensa-lx106-elf/bin;C:/msys32/usr/bin;C:/msys32/mingw32/bin";
-export PATH="C:/msys32/xtensa-lx106-elf/bin;C:/msys32/usr/bin;C:/msys32/mingw32/bin"
-make clean
+export PATH="$PATH:$HOME/esp/xtensa-lx106-elf/bin"
+
+echo "======================================================================="
+echo "UPDATING LIBRARY FILES"
+echo "======================================================================="
+
+# Sprawdzenie i tworzenie folderu "user"
+if [ ! -d "user" ]; then
+    mkdir user
+    echo "Folder 'user' created"
+else
+    echo "Folder 'user' exists"
+fi
+
+# Kopiowanie pliku lcd_hd44780.c do folderu user
+cp "../../../src/lcd_hd44780.c" "user/"
+
+# Sprawdzenie i tworzenie folderu "include"
+if [ ! -d "include" ]; then
+    mkdir include
+    echo "Folder 'include' created"
+else
+    echo "Folder 'include' exists"
+fi
+
+# Kopiowanie plików do folderu include
+echo "Copy files to include/lcd_hd44780/"
+cp "../../../src/lcd_hd44780.h" "include/"
+cp "../../../src/lcd_hd44780_GPIO_interface.h" "include/"
+cp "../../../src/lcd_hd44780_def_char.h" "include/"
+cp "../../../src/lcd_hd44780_config.h" "include/"
+cp "../../../src/lcd_hd44780_driver_commands.h" "include/"
+
+echo "======================================================================="
+echo "BUILD PROJECT"
+echo "======================================================================="
+
+# Wywołanie polecenia make
 make COMPILE=gcc BOOT=new APP=1 SPI_SPEED=40 SPI_MODE=DIO SPI_SIZE_MAP=6 all
